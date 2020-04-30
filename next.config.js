979f057ed5f6c8dfa750458cfce01533fc8ec52a
Purvi <pandyapurvi13@gmail.com>
2020-04-30 17:40:42 +1000

    setup mongoDB

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV: "mongodb+srv://purvi:<12345!@#$%>@cluster0-hxsvm.mongodb.net/test?retryWrites=true&w=majority",
+    MONGO_SRV: "mongodb+srv://purvi:12345%21%40%23%24%25@cluster0-hxsvm.mongodb.net/test?retryWrites=true&w=majority",
     JWT_SECRET: "<insert-jwt-secret>",
     CLOUDINARY_URL: "<insert-cloudinary-url>",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
