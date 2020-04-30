d4c85ae6457835940b35f8eebac49feded6df12d
Purvi <pandyapurvi13@gmail.com>
2020-04-30 14:04:33 +1000

    added a Header component

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,5 +1,99 @@
+import { Menu, Container, Image, Icon } from "semantic-ui-react";
+import Link from "next/link";
+import Router, { useRouter } from "next/router";
+
+Router.onRouterChangeStart = () => Nprogress.start();
+Router.onRouterChangeComplete = () => Nprogress.done();
+Router.onRouterChangeError = () => Nprogress.done();
+
+
 function Header() {
-  return <>Header</>;
+  const user = false;
+  const router = useRouter();
+  //router.pathname = it gives on which path we are on
+
+  const isActive = (route) => {
+    return route === router.pathname //This will return true if matches
+};
+
+  return <>
+    <Menu id="menu" inverted>
+      <Container text>
+        <Link href="/">
+          <Menu.Item header active={isActive('/')}>
+            <Image
+              size="mini"
+              src="/static/logo.svg"
+              style={{ marginRight: '1em' }}
+            />
+            ReactHouseHold
+          </Menu.Item>
+        </Link>
+
+        <Link href="/cart">
+          <Menu.Item header active={isActive('/cart')}>
+            <Icon
+              name="cart"
+              size="large"
+            />
+            Cart
+          </Menu.Item>
+        </Link>
+
+        { user && <Link href="/create">
+          <Menu.Item header active={isActive('/create')}>
+            <Icon
+              name="add square"
+              size="large"
+            />
+            Create
+          </Menu.Item>
+        </Link>}
+
+        {user ? ( <>
+          <Link href="/account">
+            <Menu.Item header active={isActive('/account')}>
+              <Icon
+                name="user"
+                size="large"
+              />
+              Account
+          </Menu.Item>
+          </Link>
+          
+          <Menu.Item header>
+            <Icon
+              name="sign out"
+              size="large"
+            />
+            Logout
+        </Menu.Item>
+        </>)
+          :
+            ( <>
+            <Link href="/login">
+              <Menu.Item header active={isActive('/login')}>
+                <Icon
+                  name="sign in"
+                  size="large"
+                />
+                Login
+          </Menu.Item>
+            </Link>
+
+            <Link href="/signup">
+              <Menu.Item header active={isActive('/signup')}>
+                <Icon
+                  name="signup"
+                  size="large"
+                />
+                Signup
+          </Menu.Item>
+            </Link>
+          </>)}
+      </Container>
+    </Menu>
+  </>;
 }
 
 export default Header;
